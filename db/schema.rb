8ef2013a89192c07ae71c608146310b9b7551022
momchiltues19 <mom.todorov.tues19@gmail.com>
2018-01-06 18:20:59 +0200

    psql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171219165013) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "rsas", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", null: false
