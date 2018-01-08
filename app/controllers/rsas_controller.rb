8309599d9a0bfee50080e88332891902b956698d
momchiltues19 <mom.todorov.tues19@gmail.com>
2018-01-08 20:10:18 +0200

    hew

--- app/controllers/rsas_controller.rb
+++ app/controllers/rsas_controller.rb
@@ -1,26 +1,21 @@
 require 'openssl'
 
 class RsasController < ApplicationController
-  before_action :set_rsa, only: %i[show edit update destroy]
+	protect_from_forgery except: :create
+	def index
+	end
 
-  def create
-    key = OpenSSL::PKey::RSA.new 2048
-    if params[:n] && params[:e] && params[:d] then set_ned key end
-    @rsa = Rsa.create(n: key.params["n"], e: key.params["e"], d: key.params["d"])
-    respond_to :json
-  end
-
-  def set_ned key
-      key.n = params[:n].to_i
-      key.e = params[:e].to_i
-      key.d = params[:d].to_i
-  end
-
-  def show
-    respond_to :json
-  end
-
-  def set_rsa
-    @rsa = Rsa.find_by(id: params[:id])
-  end
+	def create
+		if params[:n] && params[:e] && params[:d] then 
+			key.n = params[:n].to_i
+			key.e = params[:e].to_i
+			key.d = params[:d].to_i
+		end
+		else
+			key = OpenSSL::PKey::RSA.new 2048
+		end	
+		@RSA = Rsa.new(n: key.n, e: key.e, d: key.d)
+		@RSA.save
+		redirect_to @RSA
+	end
 end
