f0591bf35b32aecc797ac5222b7caee1db375d91
momchiltues19 <mom.todorov.tues19@gmail.com>
2018-01-08 20:08:10 +0200

    bew

--- app/controllers/rsas_controller.rb
+++ app/controllers/rsas_controller.rb
@@ -1,29 +1,26 @@
 require 'openssl'
 
 class RsasController < ApplicationController
-	protect_from_forgery except: :create
-	def index
-	end
+  before_action :set_rsa, only: %i[show edit update destroy]
 
-	def create
-		if params[:n] && params[:e] && params[:d] then 
-			key.n = params[:n].to_i
-			key.e = params[:e].to_i
-			key.d = params[:d].to_i
-		end
-		else
-			key = OpenSSL::PKey::RSA.new 2048
-		end	
-		@RSA = Rsa.new(n: key.n, e: key.e, d: key.d)
-		@RSA.save
-		redirect_to @RSA
-	end
+  def create
+    key = OpenSSL::PKey::RSA.new 2048
+    if params[:n] && params[:e] && params[:d] then set_ned key end
+    @rsa = Rsa.create(n: key.params["n"], e: key.params["e"], d: key.params["d"])
+    respond_to :json
+  end
 
-	def show
-		par = Hash.new
-		par[:n] = key.n
-		par[:e] = key.e
-		par[:d] = key.d
-		render json: par
-	end
+  def set_ned key
+      key.n = params[:n].to_i
+      key.e = params[:e].to_i
+      key.d = params[:d].to_i
+  end
+
+  def show
+    respond_to :json
+  end
+
+  def set_rsa
+    @rsa = Rsa.find_by(id: params[:id])
+  end
 end
